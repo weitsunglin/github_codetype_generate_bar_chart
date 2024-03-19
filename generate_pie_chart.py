3550c6d2902b297c16abdce6942d1cce4d8c8bd8
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-03-19 10:48:34 +0800

    Update generate_pie_chart.py

--- generate_pie_chart.py
+++ generate_pie_chart.py
@@ -25,6 +25,9 @@ def generate_pie_chart(languages):
     
     sizes = filtered_languages.values()
     total = sum(sizes)
+    # 计算每种语言的贡献比例，并确保该比例大于0.0%
+    filtered_languages = {lang: lines for lang, lines in filtered_languages.items() if (lines / total) > 0.0}
+
     labels = [f'{lang} {size/total:.1%}' for lang, size in filtered_languages.items()]
     
     explode = [0.1] * len(labels)  # 'Explode' all slices slightly to give a 3D effect
@@ -41,6 +44,7 @@ def generate_pie_chart(languages):
     
     plt.tight_layout()
     plt.savefig('code_exp.png')  # Save the figure as a file
+
 
 languages = fetch_languages(GITHUB_USER)
 generate_pie_chart(languages)
