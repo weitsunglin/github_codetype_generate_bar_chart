820de5dfb1817a64cd872a47d74694f8adfa34b8
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-05-21 08:52:44 +0800

    Update generate_pie_chart.py

--- generate_pie_chart.py
+++ generate_pie_chart.py
@@ -8,7 +8,8 @@ def fetch_languages(user):
     languages = {}
     repos = requests.get(f'https://api.github.com/users/{user}/repos').json()
     for repo in repos:
-        if repo['fork'] is False and repo['name'] != 'ios_line_sdk':  # Ignore forks and specific repo
+        if repo['fork'] is False and repo['name'] != 'ios_line_sdk'
+            and repo['name'] != 'llvm-mingw-20240518-msvcrt-x86_64':  # Ignore forks and specific repo
             lang_url = repo['languages_url']
             lang_data = requests.get(lang_url).json()
             for lang, lines in lang_data.items():
